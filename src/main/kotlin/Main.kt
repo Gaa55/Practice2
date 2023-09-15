931f90037f1ddca2a712faf852826220f9196864
Gaa55 <122674097+Gaa55@users.noreply.github.com>
2023-09-15 10:50:58 +0300

    DnD

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,33 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    // Задача 1:
+    val age1: Int = 42
+    val age2: Int = 21
+
+// Задача 2:
+    val avg1: Double = (age1 + age2) / 2.0
+
+// Задача 4:
+    val firstName: String = "Иван"
+    val lastName: String = "Иванов"
+
+// Задача 5:
+    val fullName: String = "$firstName"+ "$lastName"
+
+// Задача 6:
+    val myDetails: String = "Привет, меня зовут $fullName."
+
+// Задача 7:
+    val date: Triple<Int, Int, Int> = Triple(9, 14, 2023)
+
+// Задача 8:
+    val (month, day, year) = date
+
+// Задача 9:
+    val (extractedMonth, extractedYear) = date
+
+// Задача 10:
+    val updatedMonth = 10
+    val updatedDate: Pair<Int, Int> = Pair(updatedMonth, extractedYear)
+    println()
 }
